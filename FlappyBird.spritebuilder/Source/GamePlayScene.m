078dc208bcd68b309faed8d277b3228db4304b16
Wanchao Liang <wanchaol.cs@gmail.com>
2015-01-19 18:50:13 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,24 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f) {
+        //add a new obstacle
+        [self addObstacle];
+        
+        //reset the timer
+        timeSinceObstacle = 0.0f;
+    }
+    
+    
+    
 }
 
 // put new methods here
